b611243ed13dbc75826c5fc3b0ad81c5e8ef5b4d
Evan Pierzina <ekpierzina@gmail.com>
2016-04-02 23:23:41 -0700

    Connection class can now connect to the server. Set up the methods to send proper messages to the server.

--- BittleOptions/src/org/bittle/options/Connection.java
+++ BittleOptions/src/org/bittle/options/Connection.java
@@ -1,115 +1,165 @@
 package org.bittle.options;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import javax.net.ssl.SSLSocket;
-import javax.net.ssl.SSLSocketFactory;
+import java.net.URI;
+import java.util.concurrent.Future;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketError;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.eclipse.jetty.websocket.client.WebSocketClient;
 
+@WebSocket
 public class Connection {
     
-    private OutputStream outputStream;
-    private InputStream inputStream;
+    private static final Logger LOG = Log.getLogger(Connection.class);
+    private static Session session;
     
-    //might be worthwhile to put together a message/response structure
-    
-    public enum Message {
-        REGISTER,
-        LOGIN,
-        CHANGEPASSWORD,
-        CHANGEEMAIL,
-        RESETPASSWORD,
-        CREATEPROJECT,
-        DELETEPROJECT,
-        DELETEPROJECTCONFIRM,
-        ADDPERMISSION,
-        ADDPERMISSIONCONFIRM,
-        REMOVEPERMISSION,
-        CREATEBRANCH,
-        DELETEBRANCH,
-        EDITBRANCH,
-        SYNCPROJECT,
-        SYNCFILE,
-        SYNCLINE
+    @OnWebSocketConnect
+    public void onConnect(Session sess)
+    {
+        LOG.info("onConnect({})",sess);
+    }
+
+    @OnWebSocketClose
+    public void onClose(int statusCode, String reason)
+    {
+        LOG.info("onClose({}, {})", statusCode, reason);
+    }
+
+    @OnWebSocketError
+    public void onError(Throwable cause)
+    {
+        LOG.warn(cause);
+    }
+
+    @OnWebSocketMessage
+    public void onMessage(String msg)
+    {
+        LOG.info("onMessage() - {}", msg);
+        //big switch statement goes here based on the response's id
     }
     
-    public void connect(String serverAddress, int serverPort) {
+    public void connect(String url) {
+//        String url = "wss://notextures.io:8086";
+
+        SslContextFactory sslContextFactory = new SslContextFactory();
+        sslContextFactory.setTrustAll(true); // Good for testing, but make sure to change later
+
+        WebSocketClient client = new WebSocketClient(sslContextFactory);
+        try
+        {
+            client.start();
+            Connection socket = new Connection();
+            Future<Session> fut = client.connect(socket,URI.create(url));
+            session = fut.get();
+//            session.getRemote().sendString("{\"id\": \"clean\"}");
+        }
+        catch (Throwable t)
+        {
+            LOG.warn(t);
+        }
+    }
+    
+    public void close() {
+        session.close();
+    }
+    
+    private void sendMessage(String message) {
         try {
-            SSLSocket socket = (SSLSocket)((SSLSocketFactory) SSLSocketFactory.getDefault()).createSocket(serverAddress, serverPort);
-            outputStream = socket.getOutputStream();
-            inputStream = socket.getInputStream(); //might block on initialization? if so just initialize at first use
+            session.getRemote().sendString(message);
         }
-        catch(Exception e) {
-            System.out.println(e);
-        }
-    }
-    
-    
-    public void sendMessage(Message messageType, Object information) {
-        switch(messageType) {
-            case REGISTER:
-                
-                break;
-            case LOGIN:
-                
-                break;
-            case CHANGEPASSWORD:
-                
-                break;
-            case CHANGEEMAIL:
-                
-                break;
-            case RESETPASSWORD:
-                
-                break;
-            case CREATEPROJECT:
-                
-                break;
-            case DELETEPROJECT:
-                
-                break;
-            case DELETEPROJECTCONFIRM:
-                
-                break;
-            case ADDPERMISSION:
-                
-                break;
-            case ADDPERMISSIONCONFIRM:
-                
-                break;
-            case REMOVEPERMISSION:
-                
-                break;
-            case CREATEBRANCH:
-                
-                break;
-            case DELETEBRANCH:
-                
-                break;
-            case EDITBRANCH:
-                
-                break;
-            case SYNCPROJECT:
-                
-                break;
-            case SYNCFILE:
-                
-                break;
-            case SYNCLINE:
-                
-                break;
-        }
-        
-    }
-    
-    public void receiveMessage() {
-        //only if we can distinguish what a message is in response to,
-        //otherwise read within the switch statement of sendMessage
-        try {
-            inputStream.read();
-        }
-        catch(Exception e) {
-            System.out.println(e);
+        catch (Throwable t)
+        {
+            LOG.warn(t);
         }
     }
     
+    public void register(String name, String pass) {
+        sendMessage("{\"id\":\"register\", \"name\":\"" + name + "\", \"pass\":\"" + pass + "\"}");
+    }
+    
+    public void login(String name, String pass) {
+        sendMessage("{\"id\":\"login\", \"name\":\"" + name + "\", \"pass\":\"" + pass + "\"}");
+    }
+    
+    public void logout() {
+        sendMessage("{\"id\":\"logout\"}");
+    }
+    
+    public void changePass(String name, String pass, String newPass) {
+        sendMessage("{\"id\":\"changePass\", \"name\":\"" + name + "\", \"pass\":\"" + pass + "\", \"newPass\":\"" + newPass + "\"}");
+    }
+    
+    public void changeEmail(String name, String pass, String newEmail) {
+        sendMessage("{\"id\":\"changeEmail\", \"name\":\"" + name + "\", \"pass\":\"" + pass + "\", \"newEmail\":\"" + newEmail + "\"}");
+    }
+    
+    public void createRepo(String name) {
+        sendMessage("{\"id\":\"createRepo\", \"name\":\"" + name + "\"}");
+    }
+    
+    public void setPermission(String repo, String user, String role) {
+        sendMessage("{\"id\":\"setPermission\", \"repo\":\"" + repo + "\", \"user\":\"" + user + "\", \"role\":\"" + role + "\"}");
+    }
+    
+    public void deletePermission(String repo, String user) {
+        sendMessage("{\"id\":\"deletePermission\", \"repo\":\"" + repo + "\", \"user\":\"" + user + "\"}");
+    }
+    
+    public void createFile(String repo, String file) {
+        sendMessage("{\"id\":\"createFile\", \"repo\":\"" + repo + "\", \"file\":\"" + file + "\"}");
+    }
+    
+    public void moveFile(String repo, String file, String newPath) {
+        sendMessage("{\"id\":\"moveFile\", \"repo\":\"" + repo + "\", \"file\":\"" + file + "\", \"newPath\":\"" + newPath + "\"}");
+    }
+    
+    public void deleteFile(String repo, String file) {
+        sendMessage("{\"id\":\"deleteFile\", \"repo\":\"" + repo + "\", \"file\":\"" + file + "\"}");
+    }
+    
+    public void createDirectory(String repo, String directory) {
+        sendMessage("{\"id\":\"createDirectory\", \"repo\":\"" + repo + "\", \"directory\":\"" + directory + "\"}");
+    }
+    
+    public void moveDirectory(String repo, String directory, String newPath) {
+        sendMessage("{\"id\":\"moveDirectory\", \"repo\":\"" + repo + "\", \"directory\":\"" + directory + "\", \"newPath\":\"" + newPath + "\"}");
+    }
+    
+    public void deleteDirectory(String repo, String directory) {
+        sendMessage("{\"id\":\"deleteDirectory\", \"repo\":\"" + repo + "\", \"directory\":\"" + directory + "\"}");
+    }
+    
+    public void insert(String repo, String file, String lineId, String col, String data) {
+        sendMessage("{\"id\":\"insert\", \"repo\":\"" + repo + "\", \"file\":\"" + file + "\", \"lineId\":\"" + lineId + "\", \"col\": \"" + col + "\", \"data\":\"" + data + "\"}");
+    }
+    
+    public void getLine(String repo, String file, String lineId) {
+        sendMessage("{\"id\":\"getLine\", \"repo\":\"" + repo + "\", \"file\":\"" + file + "\", \"lineId\":\"" + lineId + "\"}");
+    }
+    
+    public void erase(String repo, String file, String lineId, String col, String count) {
+        sendMessage("{\"id\":\"erase\", \"repo\":\"" + repo + "\", \"file\":\"" + file + "\", \"lineId\":\"" + lineId + "\", \"col\": \"" + col + "\", \"count\":\"" + count + "\"}");
+    }
+    
+    public void split(String repo, String file, String lineId, String col, String newLineId) {
+        sendMessage("{\"id\":\"insert\", \"repo\":\"" + repo + "\", \"file\":\"" + file + "\", \"lineId\":\"" + lineId + "\", \"col\": \"" + col + "\", \"newLineId\":\"" + newLineId + "\"}");
+    }
+    
+    public void merge(String repo, String file, String lineId) {
+        sendMessage("{\"id\":\"merge\", \"repo\":\"" + repo + "\", \"file\":\"" + file + "\", \"lineId\":\"" + lineId + "\"}");
+    }
+    
+    public void getFile(String repo, String file) {
+        sendMessage("{\"id\":\"getFile\", \"repo\":\"" + repo + "\", \"file\":\"" + file + "\"}");
+    }
+    
+    public void clean() {
+        sendMessage("{\"id\":\"clean\"}");
+    }
 }
---
+++ BittleOptions/src/org/bittle/options/Tester.java
@@ -0,0 +1,20 @@
+package org.bittle.options;
+
+public class Tester {
+    
+    
+    public static void main(String[] args)
+    {
+        Connection connection = new Connection();
+        connection.connect("wss://notextures.io:8086");
+        
+        connection.register("test_evan", "tacosaregreat");
+        connection.login("test_evan", "tacosaregreat");
+        connection.changePass("test_evan", "tacosaregreat", "tacosarebad");
+        connection.logout();
+        connection.login("test_evan", "tacosaregreat");
+        
+//        connection.clean();
+//        connection.close();
+    }
+}
