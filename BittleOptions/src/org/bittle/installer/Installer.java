3248a4ebc047ac99aaf96f62530b3c64f972074e
Evan Pierzina <evanpierzina@bitbucket.org>
2016-04-23 14:24:52 -0700

    The document listeners are now properly removed when the document changes.

--- BittleOptions/src/org/bittle/installer/Installer.java
+++ BittleOptions/src/org/bittle/installer/Installer.java
@@ -14,111 +14,123 @@ import org.netbeans.api.editor.EditorRegistry;
 import org.openide.windows.WindowManager;
 
 public class Installer extends org.openide.modules.ModuleInstall implements Runnable {
-        
+
+    private Document currentDocument;
+    private DocumentListener currentDocumentListener;
+    private JTextComponent currentTextComponent;
+
     @Override
     public void restored() {
-        
+
         WindowManager manager = WindowManager.getDefault();
         manager.invokeWhenUIReady(this);
     }
 
     @Override
     public void run() {
-        
-        PropertyChangeListener l = (PropertyChangeEvent evt) -> {
-            JTextComponent jtc = EditorRegistry.lastFocusedComponent();
-            if (jtc != null) {
-                Document d = jtc.getDocument();
-                //document listener test
-                d.addDocumentListener( new DocumentListener() {
-                    public void changedUpdate( DocumentEvent e )
-                    {
-                        System.out.println( "changedUpdate: Added " + e.getLength() + 
-                          " characters, document length = " + e.getDocument().getLength() );
-                    }
 
-                    public void insertUpdate( DocumentEvent e )
-                    {
-                      System.out.println( "insertUpdate: Added " + e.getLength() + 
-                          " characters, document length = " + e.getDocument().getLength() );
-                    }
+        PropertyChangeListener l = new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                JTextComponent jtc = EditorRegistry.lastFocusedComponent();
+                //make sure we're changing editors before doing anything
+                if (currentTextComponent == null || jtc != currentTextComponent) {
+                    if (jtc != null) {
+                        currentTextComponent = jtc;
+                        if (currentDocumentListener != null) {
+                            currentDocument.removeDocumentListener(currentDocumentListener); //remove last listener from last document on changing editor
+                        }
+                        currentDocument = jtc.getDocument();
 
-                    public void removeUpdate( DocumentEvent e )
-                    {
-                      System.out.println( "removeUpdate: Removed " + e.getLength() +
-                          " characters, document length = " + e.getDocument().getLength() );
-                    }
-                });
-                //key listener test
-                jtc.addKeyListener(new KeyListener() {
-                    
-                @Override
-                public void keyTyped(KeyEvent e) {
-                    int keycode = e.getKeyCode();
-                    switch (keycode) {
-                        case KeyEvent.VK_LEFT:
-//                            JOptionPane.showMessageDialog(null, "1 Left: " + e.getKeyCode());
-                            break;
-                        case KeyEvent.VK_RIGHT:
-//                            JOptionPane.showMessageDialog(null, "1 Right: " + e.getKeyCode());
-                            break;
-                        case KeyEvent.VK_DOWN:
-//                            JOptionPane.showMessageDialog(null, "1 Down: " + e.getKeyCode());
-                            break;
-                        case KeyEvent.VK_UP:
-//                            JOptionPane.showMessageDialog(null, "1 Up: " + e.getKeyCode());
-                            break;
-                        case KeyEvent.VK_ENTER:
-//                            JOptionPane.showMessageDialog(null, "Enter: "+e.getKeyCode());
-                            break;
+                        //IF THE DOCUMENT IS ONE BEING TRACKED:
+                        currentDocument.addDocumentListener(currentDocumentListener = new DocumentListener() {
+                            public void changedUpdate(DocumentEvent e) {
+                                JOptionPane.showMessageDialog(null, "changedUpdate: Changed " + e.getLength()
+                                        + " characters, document length = " + e.getDocument().getLength());
+                            }
+
+                            public void insertUpdate(DocumentEvent e) {
+                                JOptionPane.showMessageDialog(null, "insertUpdate: Added " + e.getLength()
+                                        + " characters, document length = " + e.getDocument().getLength());
+                            }
+
+                            public void removeUpdate(DocumentEvent e) {
+                                JOptionPane.showMessageDialog(null, "removeUpdate: Removed " + e.getLength()
+                                        + " characters, document length = " + e.getDocument().getLength());
+                            }
+                        });
+                        //key listener test
+                        jtc.addKeyListener(new KeyListener() {
+
+                            @Override
+                            public void keyTyped(KeyEvent e) {
+                                int keycode = e.getKeyCode();
+                                switch (keycode) {
+                                    case KeyEvent.VK_LEFT:
+                                        //                            JOptionPane.showMessageDialog(null, "1 Left: " + e.getKeyCode());
+                                        break;
+                                    case KeyEvent.VK_RIGHT:
+                                        //                            JOptionPane.showMessageDialog(null, "1 Right: " + e.getKeyCode());
+                                        break;
+                                    case KeyEvent.VK_DOWN:
+                                        //                            JOptionPane.showMessageDialog(null, "1 Down: " + e.getKeyCode());
+                                        break;
+                                    case KeyEvent.VK_UP:
+                                        //                            JOptionPane.showMessageDialog(null, "1 Up: " + e.getKeyCode());
+                                        break;
+                                    case KeyEvent.VK_ENTER:
+                                        //                            JOptionPane.showMessageDialog(null, "Enter: "+e.getKeyCode());
+                                        break;
+                                }
+                            }
+
+                            @Override
+                            public void keyPressed(KeyEvent e) {
+                                int keycode = e.getKeyCode();
+                                switch (keycode) {
+                                    case KeyEvent.VK_LEFT:
+                                        //                            JOptionPane.showMessageDialog(null, "2 Left: " + e.getKeyCode());
+                                        break;
+                                    case KeyEvent.VK_RIGHT:
+                                        //                            JOptionPane.showMessageDialog(null, "2 Right: " + e.getKeyCode());
+                                        break;
+                                    case KeyEvent.VK_DOWN:
+                                        //                            JOptionPane.showMessageDialog(null, "2 Down: " + e.getKeyCode());
+                                        break;
+                                    case KeyEvent.VK_UP:
+                                        //                            JOptionPane.showMessageDialog(null, "2 Up: " + e.getKeyCode());
+                                        break;
+                                    case KeyEvent.VK_ENTER:
+                                        //                            JOptionPane.showMessageDialog(null, "Enter: "+e.getKeyCode());
+                                        break;
+                                }
+                            }
+
+                            @Override
+                            public void keyReleased(KeyEvent e) {
+                                int keycode = e.getKeyCode();
+                                switch (keycode) {
+                                    case KeyEvent.VK_LEFT:
+                                        //                            JOptionPane.showMessageDialog(null, "3 Left: " + e.getKeyCode());
+                                        break;
+                                    case KeyEvent.VK_RIGHT:
+                                        //                            JOptionPane.showMessageDialog(null, "3 Right: " + e.getKeyCode());
+                                        break;
+                                    case KeyEvent.VK_DOWN:
+                                        //                            JOptionPane.showMessageDialog(null, "3 Down: " + e.getKeyCode());
+                                        break;
+                                    case KeyEvent.VK_UP:
+                                        //                            JOptionPane.showMessageDialog(null, "3 Up: " + e.getKeyCode());
+                                        break;
+                                    case KeyEvent.VK_ENTER:
+                                        //                            JOptionPane.showMessageDialog(null, "Enter: "+e.getKeyCode());
+                                        break;
+                                }
+                            }
+
+                        });
                     }
                 }
-
-                @Override
-                public void keyPressed(KeyEvent e) {
-                    int keycode = e.getKeyCode();
-                    switch (keycode) {
-                        case KeyEvent.VK_LEFT:
-                            JOptionPane.showMessageDialog(null, "2 Left: " + e.getKeyCode());
-                            break;
-                        case KeyEvent.VK_RIGHT:
-                            JOptionPane.showMessageDialog(null, "2 Right: " + e.getKeyCode());
-                            break;
-                        case KeyEvent.VK_DOWN:
-                            JOptionPane.showMessageDialog(null, "2 Down: " + e.getKeyCode());
-                            break;
-                        case KeyEvent.VK_UP:
-                            JOptionPane.showMessageDialog(null, "2 Up: " + e.getKeyCode());
-                            break;
-                        case KeyEvent.VK_ENTER:
-                            JOptionPane.showMessageDialog(null, "Enter: "+e.getKeyCode());
-                            break;
-                    }
-                }
-
-                @Override
-                public void keyReleased(KeyEvent e) {
-                    int keycode = e.getKeyCode();
-                    switch (keycode) {
-                        case KeyEvent.VK_LEFT:
-                            JOptionPane.showMessageDialog(null, "3 Left: " + e.getKeyCode());
-                            break;
-                        case KeyEvent.VK_RIGHT:
-                            JOptionPane.showMessageDialog(null, "3 Right: " + e.getKeyCode());
-                            break;
-                        case KeyEvent.VK_DOWN:
-                            JOptionPane.showMessageDialog(null, "3 Down: " + e.getKeyCode());
-                            break;
-                        case KeyEvent.VK_UP:
-                            JOptionPane.showMessageDialog(null, "3 Up: " + e.getKeyCode());
-                            break;
-                        case KeyEvent.VK_ENTER:
-                            JOptionPane.showMessageDialog(null, "Enter: "+e.getKeyCode());
-                            break;
-                    }
-                }
-
-            });
             }
         };
 
